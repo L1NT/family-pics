c008bd1413e15865dea65206575ff50c21ecdaf4
Luke <luke.jeter@gmail.com>
2012-12-23 19:59:49 -0700

    added required Fancybox library and started work on transitioning site to being a webservice

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 picture-webservice
 ==================
+
+What it does:
+Turns any file directory containing photos and videos into a web service
+capable of serving folder names, thumbnails, and the photos/videos 
+themselves.
+
+Licensing:
+I haven't bothered to include any specific user license. Anyone is
+welcome to use and modify this code to their liking. If you make a 
+change or enhancement you think will benefit most or all users, I 
+encourage you to submit a pull request. Additionally, any 
+acknowledgement that includes my name or a link on your own pages (an 
+example can be found on the sample html page(s)) that use this code 
+would be greatly appreciated. Thanks!
+
+Usage assumptions:
+If you plan to follow these guidelines, it is assumed that you are 
+familiar with installing Python modules and configuring your webserver
+to run Python scripts and defining web-accessible files/folders.
+
+Requirements:
+- > Python 3.1
+- PIL must be installed [easiest implementation is to use 'pip PIL']
+- included html pages require jQuery and fancybox plugin
+
+Deployment:
+- Specify the correct path to a Python3 installation on your system
+  in /webservice/familypics.py
+- The Webservice folder needs to be in a web-accessible location
+- Configure the webservice/familypics.py variable to point to the root
+  of your media directory
+- If you choose to use the default html pages, set the picservice url
+  variable to point to the /webservice directory
+
+Features:
+- Any file(s) or folder(s) that should be hidden/ignored by the web
+  service can be prefaced with an underscore (_) character
+- The directory names are returned in reverse alphanumeric order because
+  I use a YYYY-MM naming scheme in my own directories and the most
+  recent folders are the most relevant (alter /webservice/familypics.py
+  [line 10], if this doesn't suit your needs)
---
+++ webservice/familypics.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python3
+# -*- coding: iso-8859-1 -*-
+
+import os, cgi, Image
+
+
+# this is originally from frameindex.py
+path = '/website/pics' #will want to call this dynamically!!
+
+def get_directories(path):
+    """Gets a list of non-hidden subdirectories.
+    
+    Args:
+        path: The root path to search.
+        
+    Returns:
+        A list of directories contained within 'path'.
+    """
+    files = os.listdir(path)
+    files.sort()
+    
+    #this stuff should be in the Javascript ajax function
+    for file in reversed(files):
+       if os.path.isdir(path + '/' + file):
+          if file[0] != '_':
+             print "<a href=\"framethumbnails.py?path=%s\" target=\"thumbnails\">%s</a><br />" % (file,file)
+
+
+# this is originaly from framethumbnails.py
+input = cgi.FieldStorage()
+webDir = '/website'
+picDir = '/pics/'
+path = input['path'].value
+size = 160, 120 #thumbnail size
+
+def get_hgroup(path):
+    #is this server stuff, or javascript stuff? probably javascript.
+    print '<h1>',path,'</h1>'
+    for file in os.listdir(webDir+picDir+path):
+       if os.path.isdir(webDir+picDir+path+'/'+file):
+          print "<a href=\"framethumbnails.py?path=%s\" target=\"thumbnails\">%s</a><br />" % (path+'/'+file,file)
+
+def media_count(path):
+    """Returns the number of media items in the directory.
+    
+    The total number of image and video items (excludes .THM and files
+    hidden with a leading underscore(_)) is returned.
+    """
+    pass
+    
+def get_thumbnails(path, quantity=-1, first_index=0):
+    """Gets a specified number of thumbnails images.
+    
+    This service returns the requested number of thumbnail images of the
+    images and videos contained within the path directory. If thumbnails
+    of the images don't already exist, they will be created.
+    
+    Args:
+        path: The directory containing the requested media items.
+        quantity: The number of thumbnails to return, or '-1' for all
+            items within the path directory.
+        first_index: 
+    """
+    print '<table border="3">'
+    mylist = []
+    
+    for file in os.listdir(webDir+picDir+path):
+       if ('.jpg' in file) or ('.JPG' in file) or ('.MPG' in file) or ('.MOV' in file) or ('.3gp' in file):
+          if file[0] != '_':
+             mylist.append(file)
+    
+    while len(mylist) > 0:
+       print "<tr>"
+       for j in range(4):
+          if len(mylist) == 0:
+             break
+          i = mylist.pop()
+          thumb = picDir+path+'/'+i[:-3]+'THM'
+          if ('.jpg' in i) or ('.JPG' in i):
+             #new stuff: taken from PIL tutorial
+             if not os.path.exists(webDir+thumb):
+                image = Image.open(webDir+picDir+path+'/'+i)
+                image.thumbnail(size)
+                image.save(webDir+thumb, "JPEG")
+             print "<td><a href=\"%s%s/%s\" class=\"fbox\" rel=\"group\"><img src=\"%s\" alt=\"%s\" title=\"%s\" /></a></td>" % (picDir,path,i,thumb,i,i)
+          elif (os.path.exists(webDir+thumb)):
+             print "<td  class=\"movie\"><a href=\"%s%s/%s\"><img src=\"%s\" alt=\"%s\" title=\"%s\" /></a></td>" % (picDir,path,i,thumb,i,i)
+          else:
+             print "<td class=\"movie\"><a href=\"%s%s/%s\">%s</td>" % (picDir,path,i,i)
+       print "</tr>"
+          
+    print "</table>"
